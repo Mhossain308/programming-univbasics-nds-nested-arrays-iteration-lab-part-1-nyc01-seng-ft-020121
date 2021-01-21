12503378f7df13a0d6eaf10b5b14851ff7105619
Mohamed Hossain <mz.hossain212@gmail.com>
2021-01-21 19:29:40 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,9 +1,11 @@
 def find_even_values(src)
-row_index = 0
+  row_index = 0
 while row_index < src.count do
   element_index= 0
-end
-row_index += 1
+  while element_index < src[row_index]+2
+  end
+  row_index += 1
+ end
 end
 
 
